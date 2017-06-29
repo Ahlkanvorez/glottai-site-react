2edb9624b06799d424833edfadc838bb5659025c
Robert Mitchell <robert.mitchell36@gmail.com>
2017-06-29 14:52:34 -0500

    Extracted a Word component from the Learn component, and a Words component from the Learn component.

--- src/learn/learn.js
+++ src/learn/learn.js
@@ -14,6 +14,7 @@ const WordInfo = props => (
         { props.word ? (
             <div>
                 <WordDefinition word={ props.word } />
+                <a href={props.word.lexicon}>Lexicon</a>
                 <Grammar conjugations={ [props.conjugation] }
                          declensions={ [props.declension] } />
             </div>
@@ -21,56 +22,55 @@ const WordInfo = props => (
     </div>
 );
 
-// { words, conjugations, declensions }
-class Learn extends React.Component {
+// { word, conjugations, declensions }
+class Word extends React.Component {
     constructor (props) {
         super(props);
-        this.state = {};
 
         this.showInfo = this.showInfo.bind(this);
         this.getConjugation = this.getConjugation.bind(this);
         this.getDeclension = this.getDeclension.bind(this);
     }
 
-    getDeclension (word) {
+    getDeclension () {
         // TODO: Retrieve the proper declension for the word.
     }
 
-    getConjugation (word) {
-        if (word.type === 'verb') {
+    getConjugation () {
+        if (this.props.word.type === 'verb') {
             const conjugations = this.props.conjugations
                 .filter(conjugation => (
-                    conjugation.name.toLocaleLowerCase() === word.conjugation.toLocaleLowerCase()
+                    conjugation.name.toLocaleLowerCase() === this.props.word.conjugation.toLocaleLowerCase()
                 ));
-            if (word.conjugation !== 'Irregular') {
+            if (this.props.word.conjugation !== 'Irregular') {
                 return conjugations[0];
             }
-            return conjugations.filter(conjugation => conjugation.dictionaryEntry === word.dictionaryEntry)[0];
+            return conjugations.filter(conjugation => conjugation.dictionaryEntry === this.props.word.dictionaryEntry)[0];
         }
         // If word is not a verb, undefined should be the result.
     }
 
-    getStem(word) {
-        if (word.type === 'verb') {
+    getStem() {
+        if (this.props.word.type === 'verb') {
             // If the word is a verb, then the dictionary entry is the principal parts,
             // and the stem is the second principal part minus the infinitive ending.
-            return word.dictionaryEntry.split(' ')[1].slice(0, -3);
-        } else if (word.type === 'noun' || word.type === 'adjective') {
+            return this.props.word.dictionaryEntry.split(' ')[1].slice(0, -3);
+        } else if (this.props.word.type === 'noun' || this.props.word.type === 'adjective') {
             // TODO: Do the same for nouns and adjectives.
         }
     }
 
-    showInfo (word) {
-        let conjugation = this.getConjugation(word);
-        let declension = this.getDeclension(word);
+    showInfo () {
+        let conjugation = this.getConjugation(this.props.word);
+        let declension = this.getDeclension(this.props.word);
         // Clone the tables to prevent alteration of the originals, and replace
         conjugation = conjugation ? JSON.parse(JSON.stringify(conjugation)) : undefined;
         declension = declension ? JSON.parse(JSON.stringify(declension)) : undefined;
         if (conjugation) {
-            let stem = this.getStem(word, conjugation);
+            let stem = this.getStem(this.props.word, conjugation);
             conjugation.data = conjugation.data.map(data => {
                 data.table = data.table.map(row => (
-                    row.map((col, index) => (index === 0 || word.conjugation === 'Irregular') ? col : (stem + col.slice(1)))
+                    row.map((col, index) => (index === 0 || this.props.word.conjugation === 'Irregular') ? col : (stem + col.slice(1)))
                 ));
                 return data;
             });
@@ -78,28 +78,49 @@ class Learn extends React.Component {
         if (declension) {
             // TODO: do the same for nouns & adjectives
         }
-        this.setState({ word, conjugation, declension });
+        this.props.onClick(this.props.word, conjugation, declension);
     }
 
     render () {
         return (
-            <div>
-                <div className="row">
-                    <h3>Dictionary</h3>
-                    <ul>
-                        { this.props.words.map(w => (
-                            <li key={w.form} onClick={() => this.showInfo(w)}>
-                                {w.form}
-                            </li>
-                        )) }
-                    </ul>
-                </div>
-                <WordInfo word={this.state.word}
-                          conjugation={this.state.conjugation}
-                          declension={this.state.declension} />
-            </div>
+            <span key={this.props.word.form} onClick={this.showInfo}>
+                { this.props.word.form }
+            </span>
         );
     }
 }
+
+class Words extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+
+        this.showWordInfo = this.showWordInfo.bind(this);
+    }
+
+    showWordInfo (word, conjugation, declension) {
+        this.setState({ wordInfo: <WordInfo word={word} conjugation={conjugation} declension={declension} /> });
+    }
+
+    render () {
+        return (
+            <span>
+                { this.props.words.map((w, index) =>
+                    <span key={w.form + '_' + index} style={{ marginRight: "0.5em" }}>
+                        <Word word={w}
+                              conjugations={this.props.conjugations}
+                              declensions={this.props.declensions}
+                              onClick={this.showWordInfo} />
+                    </span>
+                ) }
+                { this.state.wordInfo }
+            </span>
+        );
+    }
+}
+
+const Learn = props => (
+    <Words words={props.words} conjugations={props.conjugations} declensions={props.declensions} />
+);
 
 export { Learn };
